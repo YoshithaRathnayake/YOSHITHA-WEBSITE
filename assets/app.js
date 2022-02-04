ce6e7471aed098d332ea1ac5ae8c75409659b445
YOSHITHA RATHNAYAKE <Y.S.B.Rathnayake2008@gmail.com>
2022-02-04 07:09:43 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
     let userFirstName = prompt("Hi, I am Yoshitha Rathnayake. What is your Name?");
     alert("Hi, " + userFirstName + ". Welcome to my web site.");
+    window.open("index.html");
     window.open("home");
     window.close("index.html");
